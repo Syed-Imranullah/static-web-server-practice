3e23d3b336ab575d0bc077bf85269ed35a877b61
Syed <xayanmay@gmail.com>
2026-03-05 13:29:43 -0500

    commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+// the file path to the dist directory
+const pathToFrontend = path.join(__dirname, '../frontend/dist');
+
+// generate middleware using the file path
+const serveStatic = express.static(pathToFrontend);
+
+// Register the serveStatic middleware before the remaining controllers
+app.use(serveStatic);
+
+// other controllers 
